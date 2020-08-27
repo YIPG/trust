a17419fbad23280db61e5fab97b386d317cc6b53
YIPG <around.mr.child@gmail.com>
2020-08-27 11:54:58 +0900

    feat: add script for emulating tun

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cargo b --release
+sudo setcap cap_net_admin=eip $CARGO_TARGET_DIR/release/trust
+$CARGO_TARGET_DIR/release/trust &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
+wait $pid
