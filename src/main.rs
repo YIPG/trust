52949d2dce82b11a1f84a28229e6d562cbf3e6e0
YIPG <around.mr.child@gmail.com>
2020-08-27 12:28:25 +0900

    feat: parse ip packet and judge it's TCP protocol

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,28 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
 
         // if not ipv4, just ignore it
-        if proto != 0x0800 {
+        if eth_proto != 0x0800 {
             continue;
         }
-        eprintln!(
-            "read {} bytes (flags: {:x}, proto: {:x}) {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                eprintln!(
+                    "{} -> {} {}b of protocol {:x}",
+                    src,
+                    dst,
+                    p.payload_len(),
+                    proto,
+                );
+            }
+            Err(e) => eprintln!("ignore weird packet: {:?}", e),
+        }
     }
-    Ok(())
 }
